d8e37f95c315468769c6a33833a49ffca6cb4803
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-28 12:08:05 +0200

    new commit is added

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -18,8 +18,8 @@ export default function Header() {
         <Button className='lg:hidden' variant='primary' color='gray' size='sm' pill rounded><FiSearch/></Button>
         <div className='flex items-center gap-2 md:order-2'>
            <Button className='w-12 h-10 hidden sm:inline'  variant='primary' color='gray' size='sm' outline pill rounded><FaMoon /></Button>
-           <Link to='/sign-in'>
-            <Button className='  sm:inli' variant='primary' color='gray'  rounded>Sign In</Button>
+           <Link to='/sign-in' outline="">
+            <Button className='  sm:inli' variant='primary' color='gray' outline  rounded>Sign In</Button>
            </Link>
            <Navbar.Toggle className='sm:hidden' />
           </div>
