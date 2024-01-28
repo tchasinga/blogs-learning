6c013221b9a845903deeff97b2cec29e67f0db02
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-28 11:14:57 +0200

    new main code

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -14,10 +14,10 @@ export default function Header() {
             <TextInput className='w-96 hidden lg:inline' type='text' rightIcon={FiSearch}  placeholder='Search....' />
         </form>
         <Button className='lg:hidden' variant='primary' color='gray' size='sm' pill rounded><FiSearch/></Button>
-        <div className=''>
+        <div className='flex items-center gap-1'>
            <Button className='w-12 h-10 hidden sm:inline'  variant='primary' color='gray' size='sm' pill rounded><FaMoon /></Button>
            <Link to='/sign-in'>
-            <Button className='w-12 h-10 hidden sm:inline' variant='primary' color='gray' size='sm' pill rounded>Login</Button>
+            <Button className='  sm:inli' variant='primary' color='gray'  rounded>Sign In</Button>
            </Link>
         </div>
     </Navbar>
