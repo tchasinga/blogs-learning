25d705d8460b776187bdd0ba2b43b2017782c2fe
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-28 09:21:25 +0200

    new code is added now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './Pages/Home'
 import Dashboard from './Pages/Dashboard'
+import About from './Pages/About'
 
 function App() {
 
@@ -9,7 +10,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} /> 
+          <Route path="*" element={<h1>Not Found</h1>} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </div>
