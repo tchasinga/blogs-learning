106cb756b9ff833b771ae1706729d1ee2da61ed7
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-28 12:48:00 +0200

    new code

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,1 +1,31 @@
 const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("User", userSchema);
