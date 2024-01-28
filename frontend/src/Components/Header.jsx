36d77f5a53c2ec33b06728b5cd275eb1e6997915
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-28 10:17:41 +0200

    new code is added

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <Navbar className='border-b-2'>
         <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold flex items-center gap-1 dark:text-white'>
-            <span className='text-xl font-normal px-2 py-1 rounded-xl bg-gradient-to-r from-indigo-500 via-purple-100 to-pink-100'>Blog&apos;s</span>
+            <span className='text-xl font-normal px-2 py-1 rounded-xl bg-gradient-to-r font-bold from-indigo-500 via-purple-100 to-pink-100'>Blog&apos;s</span>
             <p>Learning</p>
         </Link>
     </Navbar>
