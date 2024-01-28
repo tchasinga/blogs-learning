a896fdeca02769aedd39cf2be451bbd84fabefc5
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-28 15:11:01 +0200

    new code is added

--- frontend/src/Pages/SingUp.jsx
+++ frontend/src/Pages/SingUp.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 export default function SingUp() {
@@ -5,7 +6,12 @@ export default function SingUp() {
     <div className="min-h-screen mt-20">
       <div className="">
         {/* Adding first div for left side */}
-        <div className=""></div>
+        <div className="">
+        <Link to='/' className=' font-semibold flex items-center gap-1 dark:text-white text-4xl'>
+            <span className='font-normal px-2 py-1 rounded-xl bg-gradient-to-r font-bold from-indigo-500 via-purple-100 to-pink-100'>Blog&apos;s</span>
+            <p>Learning</p>
+        </Link>
+        </div>
 
         {/* Adding second div  right side */}
         <div className=""></div>
