100f0fb33d4c1e222b3cb96e5486abdb4c969e44
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-28 09:23:40 +0200

    new code is now added in the system

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './Pages/Home'
 import Dashboard from './Pages/Dashboard'
 import About from './Pages/About'
+import SingIn from './Pages/SingIn'
+import SingUp from './Pages/SingUp'
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} /> 
           <Route path="*" element={<h1>Not Found</h1>} />
           <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SingIn/>} />
+          <Route path="/sign-up" element={<SingUp/>} />
         </Routes>
       </BrowserRouter>
     </div>
