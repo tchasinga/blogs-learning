4f7c38cce9cf1ae14987a8508f0443054a116f8d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-28 09:55:58 +0200

    new elements is added now

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,6 +1,12 @@
-  
+import { Navbar } from 'flowbite-react'
+import { Link } from 'react-router-dom'
+
 export default function Header() {
   return (
-    <div>Header</div>
+    <Navbar className='border-b-2'>
+        <Link to='/' className=''>
+            <span className='text-2xl font-bold'>Blog&apos;s</span>
+        </Link>
+    </Navbar>
   )
 }
