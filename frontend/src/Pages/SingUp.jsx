6d9733308823362cee54b551eca69d9c4344c012
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-28 15:04:36 +0200

    New code is added

--- frontend/src/Pages/SingUp.jsx
+++ frontend/src/Pages/SingUp.jsx
@@ -3,6 +3,13 @@
 export default function SingUp() {
   return (
     <div className="min-h-screen mt-20">
+      <div className="">
+        {/* Adding first div */}
+        <div className=""></div>
+
+        {/* Adding second div */}
+        <div></div>
+      </div>
     </div>
   )
 }
