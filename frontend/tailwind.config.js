cf14796b04fa7d2b5951b91554936b59b43df563
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-28 09:48:37 +0200

    mew code is added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+import flowbitePlugin from 'flowbite/plugin';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,6 +8,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbitePlugin,
+  ],
 }
 
