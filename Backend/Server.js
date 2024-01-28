9172f84f58508723a89b827db77c0a6890f6dcfc
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-28 12:26:43 +0200

    new coffe is added now

--- Backend/Server.js
+++ Backend/Server.js
@@ -9,10 +9,6 @@ const app = express();
 // Middlewares to parse JSON
 app.use(express.json());
 
-// Setup server port
-app.listen(3000, () => {
-    console.log('Server started at port 3000');
-});
 
 // Middlewares to allow CORS
 app.use(cors({
@@ -20,3 +16,22 @@ app.use(cors({
     credentials: true
 })
 );
+
+
+// Connecting to MongoDB is required 
+const urlmongoDB = process.env.MONGO_URL;
+mongoose
+  .connect(urlmongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(5000, () => {
+      console.log(
+        "Congratulations! Now you are live on MongoDB service at port:",
+        5000
+      );
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
