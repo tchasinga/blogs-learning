477cf1f1bec40b1dd298b21f035348011bef83b4
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-28 10:15:31 +0200

    new code is added

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <Navbar className='border-b-2'>
-        <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+        <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold flex items-center gap-1 dark:text-white'>
             <span className='text-xl font-normal px-2 py-1 rounded-xl bg-gradient-to-r from-indigo-500 via-purple-100 to-pink-100'>Blog&apos;s</span>
-            Learning
+            <p>Learning</p>
         </Link>
     </Navbar>
   )
