cd8f961b6c06617e40483143c33600eb3f49959c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-28 09:18:17 +0200

    new code is added now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import Dashboard from './Pages/Dashboard'
 
 function App() {
 
   return (
     <div>
-      <h1 className='text-red-500'>Balolwami jack</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
