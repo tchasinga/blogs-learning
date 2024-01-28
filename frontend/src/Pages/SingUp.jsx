da4aefa92a17d7f8dd0cfc12a5debfa99cb74117
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-28 15:18:04 +0200

    new changes

--- frontend/src/Pages/SingUp.jsx
+++ frontend/src/Pages/SingUp.jsx
@@ -7,10 +7,17 @@ export default function SingUp() {
       <div className="">
         {/* Adding first div for left side */}
         <div className="">
-        <Link to='/' className=' font-semibold flex items-center gap-1 dark:text-white text-5xl'>
+        <Link to='/' className=' py-4 font-semibold flex items-center gap-1 dark:text-white text-5xl'>
             <span className='font-normal px-2 py-1 rounded-xl bg-gradient-to-r font-bold from-indigo-500 via-purple-100 to-pink-100'>Blog&apos;s</span>
             <p>Learning</p>
         </Link>
+          <div className="text-sm text-dark-900 font-normal">
+          <p>It is important to automate, collect, manage, calculate, and</p>
+         <p>analyze the processing of data and information accurately. Programming</p>
+          <p>is the process of creating a set of instructions that tell a computer</p>
+          <p>how to perform a task. Programming can be done using a variety of</p>
+          <p>computer languages, such as JavaScript, Reactjs, Nextjs, and Tailwindcss...</p>
+          </div>
         </div>
 
         {/* Adding second div  right side */}
