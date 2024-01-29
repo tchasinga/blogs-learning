eb91bd8a86206b5a273568f37f46de1348c42500
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-29 17:57:06 +0200

    New code is added

--- frontend/src/Components/ThemeProvider.jsx
+++ frontend/src/Components/ThemeProvider.jsx
@@ -1,8 +1,16 @@
-
-
-
-export default function ThemeProvider() {
-  return (
-    <div>ThemeProvider</div>
-  )
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+export default function ThemeProvider({ children }) {
+    const { theme } = useSelector((state) => state.theme.theme);
+    return (
+        <div className={theme}>
+           <div className='bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16,23,42)]'>
+           {children}
+           </div>
+        </div>
+    )
 }
+
+ThemeProvider.propTypes = {
+    children: PropTypes.node.isRequired
+};
