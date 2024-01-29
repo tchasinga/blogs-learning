a1f1135ba28c1b39eaf5c681684e3fa46e1b61af
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-29 13:52:07 +0200

    New code is added now

--- frontend/src/Pages/OAuth.jsx
+++ frontend/src/Pages/OAuth.jsx
@@ -1,9 +1,14 @@
 import { Button } from 'flowbite-react'
 import { FcGoogle } from "react-icons/fc";
+import {GoogleAuthProvider} from 'firebase/auth'
 
 export default function OAuth() {
+    const handlerGoogleClicker = async () => {
+        const provider = new GoogleAuthProvider()
+    }
+
   return (
-    <Button type='button'>
+    <Button type='button' onClick={handlerGoogleClicker}>
        <FcGoogle className='text-2xl'/>
        <text className='ml-2'>Hang on with Google</text>
     </Button>
