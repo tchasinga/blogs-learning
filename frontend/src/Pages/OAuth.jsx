2fc38d24fa78e31b622d2b8b1dd9216794bf42e4
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-29 14:20:10 +0200

    Commit data

--- frontend/src/Pages/OAuth.jsx
+++ frontend/src/Pages/OAuth.jsx
@@ -27,7 +27,9 @@ export default function OAuth() {
         })
         const data = await res.json()
         console.log(data)  
-        dispatch(singInSuccess(data))
+         if(res.ok){
+            dispatch(singInSuccess(data))
+         }
         } catch (error) {
         console.log(error)
         }
