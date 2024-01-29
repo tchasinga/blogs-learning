5e6e523710cc5e4af3aaab2a38ddd24253c0b365
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-29 17:49:56 +0200

    new code is added

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,9 +3,11 @@ import userReducer from './user/userSlice.js'
 import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';
 import persistStore from 'redux-persist/es/persistStore.js';
+import themeReducer from './theme/themeSlice.js'
 
 const rootReducer = combineReducers({
-    user: userReducer
+    user: userReducer,
+    theme: themeReducer
 })
 
 const persistConfig = {
