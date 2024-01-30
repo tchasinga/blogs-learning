4f7bda1851adc5743ac210f752cbcd1e5d75d82e
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-30 12:14:02 +0200

    Ne wcode is added

--- frontend/src/Components/DashProfile.jsx
+++ frontend/src/Components/DashProfile.jsx
@@ -6,7 +6,7 @@ export default function DashProfile() {
 
   return ( 
     <div className=''>
-      <h1 className='text-center gap-3'>Profile</h1>
+      <h1 className='text-center pb-2'>Profile</h1>
       <form className='flex flex-col'>
             <div className='w-32 h-32 self-center cursor-pointer '>
                 <img src={currentUser.user.ProfilePhoto} className=' rounded-full w-full h-full border-8  border-[lightgray] object-cover '/>
