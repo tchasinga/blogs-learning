5bf290b4132175a671a6b0566ec695513a6e3b3b
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-30 11:00:29 +0200

    New change's

--- frontend/src/Components/DashProfile.jsx
+++ frontend/src/Components/DashProfile.jsx
@@ -1,7 +1,15 @@
-
+import { useSelector } from 'react-redux';
 
 export default function DashProfile() {
+
+  const currentUser = useSelector((state) => state.user?.user?.currentUser);
+
   return ( 
-    <div>DashProfile</div>
+    <div>
+      <h1>Profile</h1>
+        <form className=''>
+          <img src={currentUser.user.ProfilePhoto}/>
+        </form>
+    </div>
   )
 }
