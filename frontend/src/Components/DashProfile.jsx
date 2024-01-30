18a2aba405484a02786f5f8221fe243ced96aa84
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-30 12:26:56 +0200

    New code is added

--- frontend/src/Components/DashProfile.jsx
+++ frontend/src/Components/DashProfile.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { TextInput, Button } from 'flowbite-react'
 
 export default function DashProfile() {
 
@@ -10,6 +11,15 @@ export default function DashProfile() {
       <form className='flex flex-col'>
             <div className='w-32 h-32 self-center cursor-pointer '>
                 <img src={currentUser.user.ProfilePhoto} className=' rounded-full w-full h-full border-8  border-[lightgray] object-cover '/>
+            </div>
+
+           <div className='mt-5 m-2 flex flex-col gap-3'>
+           <TextInput className='w-full' type='text' id='username' label='Username' value={currentUser.user.username} />
+            <TextInput className='w-full' type='email' id='email'  label='Email' value={currentUser.user.email} />
+            <TextInput className='w-full' type='password' id='password' label='password' placeholder='************' />
+           </div>
+            <div className='flex justify-center m-2'>
+                <Button className='w-full'>Update</Button>
             </div>
         </form>
     </div>
