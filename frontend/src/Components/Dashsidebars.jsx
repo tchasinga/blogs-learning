13ff4ba2733ca4ab4d25fac46d438fe15212339d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-30 10:39:04 +0200

    new code is added now

--- frontend/src/Components/Dashsidebars.jsx
+++ frontend/src/Components/Dashsidebars.jsx
@@ -1,18 +1,34 @@
 import { Sidebar } from 'flowbite-react';
 import { HiUser , HiArrowSmRight } from "react-icons/hi";
-
+import { useEffect, useState } from "react";
+import {useLocation} from "react-router-dom";
 export default function Dashsidebars() {
+
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() =>{
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+     setTab(tabFromUrl);
+     console.log(tabFromUrl);
+     if(tabFromUrl){
+       setTab(tabFromUrl);
+     }
+  }
+   , [location.search]
+  )
+
   return (
     <Sidebar className="dashsidebars">
       <Sidebar.Items>
 
       <Sidebar.ItemGroup>
 
-        <Sidebar.Item to="/dashboard" active icon={HiUser} label="user" labelColor='dark'>
+        <Sidebar.Item to="/dashboard" active={tab === 'profile'} icon={HiUser} label="user" labelColor='dark'>
           Profile
         </Sidebar.Item>
         
-        <Sidebar.Item to="/dashboard" active icon={HiArrowSmRight}>
+        <Sidebar.Item to="/dashboard"  icon={HiArrowSmRight}>
           Sing out
         </Sidebar.Item>
         </Sidebar.ItemGroup>
