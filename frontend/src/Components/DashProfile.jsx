d0dfacbd0c93e3591a9061cf30a9fca07a0a7117
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-30 13:08:33 +0200

    Nest commit

--- frontend/src/Components/DashProfile.jsx
+++ frontend/src/Components/DashProfile.jsx
@@ -1,12 +1,13 @@
 import { useSelector } from 'react-redux';
 import { TextInput, Button } from 'flowbite-react'
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function DashProfile() {
 
   const currentUser = useSelector((state) => state.user?.user?.currentUser);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef( );
 
   const handlerImageChanges = (e) =>{
     const file = e.target.files[0];
@@ -14,17 +15,25 @@ export default function DashProfile() {
      setImageFile(file);
      setImageFileUrl(URL.createObjectURL(file));
     }
+  };
+  
+  useEffect(() => {
+    if(imageFile){
+      uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async () =>{
+    console.log('uploading image');
   }
-   console.log(imageFile, imageFileUrl);
- 
 
   return ( 
     <div className=''>
-      <h1 className='text-center pb-2'>Profile</h1>
+      <h1 className='text-center pb-2'>Profile</h1>  
       <form className='flex flex-col'>
-            <input type='file' className=''  accept='image/*' onChange={handlerImageChanges}/>
-            <div className='w-32 h-32 self-center cursor-pointer '>
-                <img src={imageFileUrl || currentUser.user.ProfilePhoto} className=' rounded-full w-full h-full border-8  border-[lightgray] object-cover '/>
+            <input type='file' className='hidden'  accept='image/*'  onChange={handlerImageChanges} ref={filePickerRef}/>
+            <div className='w-32 h-32 self-center cursor-pointer ' onClick={()=> filePickerRef.current.click()}>   
+                <img src={imageFileUrl || currentUser.user.ProfilePhoto}   className=' rounded-full w-full h-full border-8  border-[lightgray] object-cover '/>
             </div>
 
            <div className='mt-5 m-2 flex flex-col gap-3'>
