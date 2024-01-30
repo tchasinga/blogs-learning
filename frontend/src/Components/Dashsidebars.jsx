9efd55fa2669ecfe16541e4e5407a89209661ffe
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-30 10:03:23 +0200

    New code is added

--- frontend/src/Components/Dashsidebars.jsx
+++ frontend/src/Components/Dashsidebars.jsx
@@ -1,6 +1,17 @@
+import { Sidebar } from 'flowbite-react';
 
 export default function Dashsidebars() {
   return (
-    <div>Dashsidebars</div>
+    <Sidebar className="dashsidebars">
+      <Sidebar.Item className="dashsidebars-item" icon="home" title="Home" url="/dashboard" />
+      <Sidebar.Item className="dashsidebars-item" icon="user" title="Profile" url="/dashboard?tab=profile" />
+      <Sidebar.Item className="dashsidebars-item" icon="user" title="Profile" url="/dashboard?tab=profile" />
+      <Sidebar.Item className="dashsidebars-item" icon="user" title="Profile" url="/dashboard?tab=profile" />
+      <Sidebar.Item className="dashsidebars-item" icon="user" title="Profile" url="/dashboard?tab=profile" />
+      <Sidebar.Item className="dashsidebars-item" icon="user" title="Profile" url="/dashboard?tab=profile" />
+      <Sidebar.Item className="dashsidebars-item" icon="user" title="Profile" url="/dashboard?tab=profile" />
+      <Sidebar.Item className="dashsidebars-item" icon="user" title="Profile" url="/dashboard?tab=profile" />
+      <Sidebar.Item className="dashsidebars-item" icon="user" title="Profile" url="/dashboard?tab=profile" />
+    </Sidebar>
   )
 }
