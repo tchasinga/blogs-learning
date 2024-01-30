6333543e3990636bcbced8d24832507cd2e17ac1
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-30 12:07:01 +0200

    new code is added now

--- frontend/src/Components/DashProfile.jsx
+++ frontend/src/Components/DashProfile.jsx
@@ -8,7 +8,7 @@ export default function DashProfile() {
     <div className=''>
       <h1>Profile</h1>
       <form className='flex flex-col'>
-            <div className=''>
+            <div className='w-32 h-32 self-center'>
                 <img src={currentUser.user.ProfilePhoto} className=' rounded-full w-full h-full border-8  border-[lightgray] object-cover '/>
             </div>
         </form>
