6236135cf5a6d16bb62a4a377eb0bc3fd7ecfdc4
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-30 12:05:20 +0200

    new code is added now

--- frontend/src/Components/DashProfile.jsx
+++ frontend/src/Components/DashProfile.jsx
@@ -5,12 +5,12 @@ export default function DashProfile() {
   const currentUser = useSelector((state) => state.user?.user?.currentUser);
 
   return ( 
-    <div >
+    <div className=''>
       <h1>Profile</h1>
       <form className='flex flex-col'>
-          <div className='w-32 h-32 self-center'>
-             <img src={currentUser.user.ProfilePhoto} className=' rounded-full w-full h-full border-8  border-[lightgray] object-cover'/>
-          </div>
+            <div className=''>
+                <img src={currentUser.user.ProfilePhoto} className=' rounded-full w-full h-full border-8  border-[lightgray] object-cover '/>
+            </div>
         </form>
     </div>
   )
