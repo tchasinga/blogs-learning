66a76ec2b02aedab9a17212b62d44a37eff5d0c1
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-05 14:36:58 +0200

    new code is added

---
+++ Backend/models/post.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    photo: {
+        type: String,
+        required: false,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    categories: {
+        type: Array,
+        required: false,
+    },
+    }, {
+    timestamps: true,
+    });
+
+const Post = mongoose.model("Post", postSchema);
+module.exports = Post;
