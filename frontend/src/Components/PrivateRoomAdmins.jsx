10e2eff1a7793dea1efabc7a4be3e67c9a8119e6
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-05 13:11:36 +0200

    New code is added now

---
+++ frontend/src/Components/PrivateRoomAdmins.jsx
@@ -0,0 +1,7 @@
+import { useSelector } from "react-redux";
+import { Outlet, Navigate } from 'react-router-dom';
+
+export default function PrivateRoomAdmins() {
+    const currentUser = useSelector((state) => state.user && state.user.user.currentUser);
+    return currentUser.isAdmin ? <Outlet /> : <Navigate to='/sign-in' />;
+}
