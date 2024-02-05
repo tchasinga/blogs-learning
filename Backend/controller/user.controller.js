6d0288d355805678a189b3dff1eee8b154378ee7
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-05 08:50:07 +0200

    new code is added

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,30 +1,40 @@
 const User = require("../models/user.model.js");
 const bcrypt = require("bcryptjs");
 
+
+
+// adding user updating profile
 const updateUserProfile = async (req, res, next) => {
   try {
-    
-   // Check if the password meets the minimum length requirement
-if (req.body.password && req.body.password.length < 6) {
-  return res.status(401).json({ msg: "Password should be at least 6 characters long" });
-}
+    // Check if the password meets the minimum length requirement
+    if (req.body.password && req.body.password.length < 6) {
+      return res
+        .status(401)
+        .json({ msg: "Password should be at least 6 characters long" });
+    }
 
-// Hash the password if it exists and meets the length requirement
-if (req.body.password) {
-  req.body.password = await bcrypt.hash(req.body.password, 10);
-}
+    // Hash the password if it exists and meets the length requirement
+    if (req.body.password) {
+      req.body.password = await bcrypt.hash(req.body.password, 10);
+    }
 
     // Check username constraints
     if (req.body.username) {
       const usernameLength = req.body.username.length;
       if (usernameLength < 4 || usernameLength > 20) {
-        return res.status(401).json({ msg: "Username should be between 4 and 20 characters long" });
+        return res
+          .status(401)
+          .json({ msg: "Username should be between 4 and 20 characters long" });
       }
       if (req.body.username.includes(" ")) {
-        return res.status(401).json({ msg: "Username should not contain spaces" });
+        return res
+          .status(401)
+          .json({ msg: "Username should not contain spaces" });
       }
       if (!/^[a-zA-Z0-9]+$/.test(req.body.username)) {
-        return res.status(401).json({ msg: "Username should not contain special characters" });
+        return res
+          .status(401)
+          .json({ msg: "Username should not contain special characters" });
       }
     }
 
@@ -32,7 +42,9 @@ if (req.body.password) {
     if (req.body.email) {
       const emailLength = req.body.email.length;
       if (emailLength < 6 || emailLength > 30) {
-        return res.status(401).json({ msg: "Email should be between 6 and 30 characters long" });
+        return res
+          .status(401)
+          .json({ msg: "Email should be between 6 and 30 characters long" });
       }
     }
 
@@ -49,15 +61,15 @@ if (req.body.password) {
       },
       { new: true }
     );
-    
+
     if (!updatedUser) {
       console.error("No user found with this id");
       return res.status(404).json({ msg: "No user found with this id" });
     }
-    
+
     // Omit password field from the response
     const { password, ...rest } = updatedUser._doc;
-    
+
     // Send the response
     res.status(200).json({
       success: true,
