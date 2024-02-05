8b36fef1d9e0c00b7979a20bec8fdff2560e03d7
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-05 14:48:57 +0200

    get

--- Backend/models/post.model.js
+++ Backend/models/post.model.js
@@ -1,36 +1,40 @@
 const mongoose = require("mongoose");
-const postSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-   userId: {
-        type: String,
-        required: true,
-   },
+    userId: {
+      type: String,
+      required: true,
+    },
     content: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     image: {
-        type: String,
-        default : "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      type: String,
+      default:
+        "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     username: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     category: {
-        type: String,
-        default: "uncategorized",
+      type: String,
+      default: "uncategorized",
     },
     slug: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    }, {
+  },
+  {
     timestamps: true,
-    });
+  }
+);
 
 const Post = mongoose.model("Post", postSchema);
 module.exports = Post;
