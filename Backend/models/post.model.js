43d8e6cfbfad2f290e0396381a3cd0c9f6a7312c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-05 14:46:29 +0200

    new code is added now

--- Backend/models/post.model.js
+++ Backend/models/post.model.js
@@ -21,7 +21,7 @@ const postSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    categories: {
+    category: {
         type: Array,
         required: false,
     },
