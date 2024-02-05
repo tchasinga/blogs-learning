0d646051deacfb3438387f695997ba2708fde35b
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-05 13:28:16 +0200

    new code is added now

--- frontend/src/Pages/CreatePost.jsx
+++ frontend/src/Pages/CreatePost.jsx
@@ -2,7 +2,7 @@
 export default function CreatePost() {
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
-        <h1>Create Post</h1>
+        <h1 className="text-center text-2xl text-dark-700">Create Post</h1>
     </div>
   )
 }
