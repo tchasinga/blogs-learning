2f70166a7ffcef8f5eccfc50aef2791d501a1087
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-06 11:24:18 +0200

    new code is added

--- Backend/controller/post.controller.js
+++ Backend/controller/post.controller.js
@@ -1,41 +1,32 @@
 const Post = require('../models/post.model.js');
 
-
 // Create Post
-const createPost = async (req, res) => {
-    
-    console.log(req.user)
-    // if(!req.user || !req.user.isAdmin){
-    //     res.status(403).send('You are not authorized to perform this action');
-    // } 
-    if(!req.body.title || !req.body.content){
-        return res.status(400).json({message: "Title and content are required...!!!"})
-    }
+const createPost = async (req, res) => { 
+    // if (!req.user.isAdmin || !req.user.id) {
+    //     return res.status(401).json({ message: "User is not authenticated or user id is missing." });
+    //    }
+       if (!req.body.title || !req.body.content) {
+        return res.status(401).json({ message: "All content are required " });
+       }
+       const slug = req.body.title
+         .split(' ')
+         .join('-')
+         .toLowerCase()
+         .replace(/[^a-zA-Z0-9-]/g, '');
+       const newPost = new Post({
+         ...req.body,
+         slug,
+         userId: req.user.id,
+       });
+       try {
+         const savedPost = await newPost.save();
+         res.status(201).json(savedPost);
+       } catch (error) {
+         next(error);
+       }
+     };
 
-    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '');
-    const newPost = new Post({
-        ...req.body,
-        slug,
-        userId : req.user.id
-    });
-   try {
-    const savedPost =  newPost.save()
-    res.status(201).json({
-        message: "Post created successfully...!!!",
-        post: savedPost,
-        success: true
-    })
-   } catch (error) {
-    res.status(500).json({
-        message: "Post not created...!!!",
-        error: error.message,
-        success: false
-    })
-   }
-}
-
-
-// Adding export methodes
+// Adding export methods
 module.exports = {
     createPost
 }
--- Backend/models/post.model.js
+++ Backend/models/post.model.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema(
   {
-    title: {
+    userId: {
       type: String,
       required: true,
     },
-    userId: {
+    title: {
       type: String,
       required: true,
     },
     content: {
       type: String,
-      required: true, 
+      required: true,
     },
     image: {
       type: String,
