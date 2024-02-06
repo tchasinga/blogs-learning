74691a421bdb8f49555468f89b552cef3fe73b10
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-06 11:26:46 +0200

    New code is added now

--- Backend/controller/post.controller.js
+++ Backend/controller/post.controller.js
@@ -16,14 +16,11 @@ const createPost = async (req, res) => {
        const newPost = new Post({
          ...req.body,
          slug,
-         userId: req.user.id,
+        //  userId: req.user.id,
        });
-       try {
          const savedPost = await newPost.save();
          res.status(201).json(savedPost);
-       } catch (error) {
-         next(error);
-       }
+    
      };
 
 // Adding export methods
